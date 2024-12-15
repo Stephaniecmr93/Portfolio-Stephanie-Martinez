d8f85a8b44fea17b6a08689d73c1cd2b3ce9a8f3
Stephanie C. Martínez R. <111431127+Stephaniecmr93@users.noreply.github.com>
2024-12-15 18:15:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// script.js (por ahora vacío o con funcionalidades simples como animaciones)
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("El portafolio de Stephanie Martínez está listo.");
+});
